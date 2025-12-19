e1cccc475448c3d61be1662afbc1934f83cd2f6a
aanyam22 <aanyame14@proton.me>
2025-12-18 21:04:41 -0600

    first commit

---
+++ Diabetes_Prediction/models/GB_pred.py
@@ -0,0 +1,281 @@
+'''
+Import Libraries and Expand DF View
+'''
+import math
+import pandas as pd
+import numpy as np
+from operator import itemgetter
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split
+import sklearn.metrics as metrics
+
+from sklearn.ensemble import GradientBoostingClassifier
+
+import warnings
+warnings.filterwarnings("ignore")
+
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', None)
+
+TARGET_F = "Survived"
+
+TEST_FILE = "data/test.csv"
+TARGET_FILE = "data/train.csv"
+
+test_df = pd.read_csv( TEST_FILE )
+train_df = pd.read_csv( TARGET_FILE )
+
+
+'''
+View the dataframe fields and datatypes
+'''
+
+print("Train DataFrame")
+print(train_df.head().T)
+print( train_df.dtypes )
+print("\n\n\n")
+
+print("Test DataFrame")
+print( test_df.dtypes )
+print( test_df.dtypes )
+print("\n\n\n")
+
+'''
+Cleaning Train Dataframe
+'''
+
+train_dt = train_df.dtypes
+
+train_objList = []
+train_intList = []
+train_floatList = []
+train_numList = []
+
+for i in train_dt.index :
+   if i in ( [ TARGET_F] ) : continue
+   if train_dt[i] in (["object"]) : train_objList.append( i )
+   if train_dt[i] in (["float64"]) : train_floatList.append( i )
+   if train_dt[i] in (["int64"]) : train_intList.append( i )
+   if train_dt[i] in (["float64","int64"]) : train_numList.append( i )
+
+# Dealing with Missing Values
+for i in train_numList :
+    if train_df[i].isna().sum() == 0 : continue
+    FLAG = "M_" + i
+    IMP = "IMP_" + i
+    train_df[ FLAG ] = train_df[i].isna() + 0
+    train_df[ IMP ] = train_df[ i ]
+    train_df.loc[ train_df[IMP].isna(), IMP ] = train_df[i].median()
+    train_df = train_df.drop( i, axis=1 )
+
+print( train_df.describe().T)
+print(train_df.head())
+
+# One Hot Encoding
+imputedList = ["Sex", "Embarked"]
+
+for i in imputedList :
+    thePrefix = "z_" + i
+    y = pd.get_dummies( train_df[i], dtype = int, prefix=thePrefix, dummy_na=False)   
+    train_df = pd.concat( [train_df, y], axis=1 )
+    train_df = train_df.drop( i, axis=1 )
+    
+print( train_df.describe().T)
+print(train_df.head())
+
+#Dropping Remaining Object Types
+train_dt = train_df.dtypes
+train_numList = []
+train_objList = []
+
+for i in train_dt.index :
+    if i in ( [ TARGET_F ] ) : continue
+    if train_dt[i] in (["object"]) : train_objList.append( i )
+    if train_dt[i] in (["float64","int64"]) : train_numList.append( i )
+
+for i in train_objList:
+    train_df = train_df.drop( i, axis=1 )
+
+train_df = train_df.drop( ["M_Age"], axis=1 )
+
+print(train_df.dtypes)
+print( train_df.describe().T)
+print(train_df.head())
+
+
+# Outlier Truncation - Disabled
+
+# for i in numList :
+#     theMean = train_df[i].mean()
+#     theSD = train_df[i].std()
+#     theMax = train_df[i].max()
+#     theCutoff = round( theMean + 3*theSD )
+#     if theMax < theCutoff : continue
+#     FLAG = "O_" + i
+#     TRUNC = "TRUNC_" + i
+#     train_df[ FLAG ] = ( train_df[i] > theCutoff )+ 0
+#     train_df[ TRUNC ] = train_df[ i ]
+#     train_df.loc[ train_df[TRUNC] > theCutoff, TRUNC ] = theCutoff
+#     train_df = train_df.drop( i, axis=1 )
+
+
+'''
+Cleaning Test Dataframe
+'''
+
+test_dt = test_df.dtypes
+
+test_objList = []
+test_intList = []
+test_floatList = []
+test_numList = []
+
+for i in test_dt.index :
+   if test_dt[i] in (["object"]) : test_objList.append( i )
+   if test_dt[i] in (["float64"]) : test_floatList.append( i )
+   if test_dt[i] in (["int64"]) : test_intList.append( i )
+   if test_dt[i] in (["float64","int64"]) : test_numList.append( i )
+
+# Dealing with Missing Values
+for i in test_numList :
+    if test_df[i].isna().sum() == 0 : continue
+    FLAG = "M_" + i
+    IMP = "IMP_" + i
+    test_df[ FLAG ] = test_df[i].isna() + 0
+    test_df[ IMP ] = test_df[ i ]
+    test_df.loc[ test_df[IMP].isna(), IMP ] = test_df[i].median()
+    test_df = test_df.drop( i, axis=1 )
+
+print( test_df.describe().T)
+print(test_df.head())
+
+
+# One Hot Encoding
+imputedList2 = ["Sex", "Embarked"]
+
+for i in imputedList2 :
+    thePrefix = "z_" + i
+    y = pd.get_dummies( test_df[i], dtype = int, prefix=thePrefix, dummy_na=False)   
+    test_df = pd.concat( [test_df, y], axis=1 )
+    test_df = test_df.drop( i, axis=1 )
+
+print( test_df.describe().T)
+print(test_df.head())
+
+#Dropping Remaining Object Types
+test_dt = test_df.dtypes
+test_numList = []
+test_objList = []
+
+for i in test_dt.index :
+    if test_dt[i] in (["object"]) : test_objList.append( i )
+    if test_dt[i] in (["float64","int64"]) : test_numList.append( i )
+
+for i in test_objList:
+    test_df = test_df.drop( i, axis=1 )
+    
+test_df = test_df.drop( ["M_Age"], axis=1 )
+test_df = test_df.drop( ["M_Fare"], axis=1 )
+
+print(test_df.dtypes)
+print( test_df.describe().T)
+print(test_df.head())
+
+
+"""
+SPLIT DATA FOR TRAINING
+"""
+features = ["z_Sex_male", "z_Sex_female", "Pclass", "Fare", "IMP_Age"]
+
+X = train_df[features].copy()
+Y = train_df[ [TARGET_F] ]
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, train_size=0.8, test_size=0.2, random_state=5)
+
+
+"""
+MODEL ACCURACY METRICS
+"""
+
+def getProbAccuracyScores( NAME, MODEL, X, Y ) :
+    pred = MODEL.predict( X )
+    probs = MODEL.predict_proba( X )
+    acc_score = metrics.accuracy_score(Y, pred)
+    p1 = probs[:,1]
+    fpr, tpr, threshold = metrics.roc_curve( Y, p1)
+    auc = metrics.auc(fpr,tpr)
+    return [NAME, acc_score, fpr, tpr, auc]
+
+def print_ROC_Curve( TITLE, LIST ) :
+    fig = plt.figure(figsize=(6,4))
+    plt.title( TITLE )
+    for theResults in LIST :
+        NAME = theResults[0]
+        fpr = theResults[2]
+        tpr = theResults[3]
+        auc = theResults[4]
+        theLabel = "AUC " + NAME + ' %0.2f' % auc
+        plt.plot(fpr, tpr, label = theLabel )
+    plt.legend(loc = 'lower right')
+    plt.plot([0, 1], [0, 1],'r--')
+    plt.xlim([0, 1])
+    plt.ylim([0, 1])
+    plt.ylabel('True Positive Rate')
+    plt.xlabel('False Positive Rate')
+    plt.show()
+
+def print_Accuracy( TITLE, LIST ) :
+    print( TITLE )
+    print( "======" )
+    for theResults in LIST :
+        NAME = theResults[0]
+        ACC = theResults[1]
+        print( NAME, " = ", ACC )
+    print( "------\n\n" )
+
+def getEnsembleTreeVars( ENSTREE, varNames ) :
+    importance = ENSTREE.feature_importances_
+    index = np.argsort(importance)
+    theList = []
+    for i in index :
+        imp_val = importance[i]
+        if imp_val > np.average( ENSTREE.feature_importances_ ) :
+            v = int( imp_val / np.max( ENSTREE.feature_importances_ ) * 100 )
+            theList.append( ( varNames[i], v ) )
+    theList = sorted(theList,key=itemgetter(1),reverse=True)
+    return theList
+
+
+"""
+GRADIENT BOOSTING MODELING
+"""
+
+WHO = "GB"
+
+CLM = GradientBoostingClassifier(max_depth=5, random_state=5)
+CLM = CLM.fit( X_train, Y_train[ TARGET_F ] )
+
+TRAIN_CLM = getProbAccuracyScores( WHO + "_Train", CLM, X_train, Y_train[ TARGET_F ] )
+TEST_CLM = getProbAccuracyScores( WHO, CLM, X_test, Y_test[ TARGET_F ] )
+
+print_ROC_Curve( WHO, [ TRAIN_CLM, TEST_CLM ] ) 
+print_Accuracy( WHO + " CLASSIFICATION ACCURACY", [ TRAIN_CLM, TEST_CLM ] )
+
+feature_cols = list( X.columns.values )
+vars_GB_flag = getEnsembleTreeVars( CLM, feature_cols )
+
+GB_CLM = TEST_CLM.copy()
+
+
+"""
+Final Prediction on Test Data
+"""
+test_df = test_df.rename( columns={ "IMP_Fare" : "Fare" } )
+test_data = test_df[features].copy()
+
+test_predictions = CLM.predict(test_data)
+
+output = pd.DataFrame({'PassengerId': test_df.PassengerId, 'Survived': test_predictions})
+output.to_csv('submissions/GB_submission.csv', index=False)
